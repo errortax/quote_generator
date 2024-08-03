c4c76601feab363286e956a299ea580c720f14e5
tasmia monzoor <tasmiamonzoor@gmail.com>
2024-08-03 22:49:33 +0600

    home_screen updated with api

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,20 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:app_1/screen/second_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+    
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            body: Center(
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const SecondScreen())
-                        );
-                  }, child: const Text('Go to next screen')),
-            )));
+      child: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              var url = Uri.parse('https://api.quotable.io/random?tags=technology,famous-quotes');
+              var response = await http.get(url);
+              var data = jsonDecode(response.body);
+
+              print("response status code: ${response.statusCode}");
+              print("response body: ${response.body}");
+
+              String quote = data['content'];
+              String author = data['author'];
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => SecondScreen(quoteText: quote, authorName: author),
+              ));
+            },
+            child: const Text('Get Quote'),
+          ),
+        ),
+      ),
+    );
   }
 }
