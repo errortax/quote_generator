6ba994aa475f9a67f5a7ee9c8756e2d1155b93f6
tasmia monzoor <tasmiamonzoor@gmail.com>
2024-08-03 22:56:56 +0600

    new button added

--- lib/screen/second_screen.dart
+++ lib/screen/second_screen.dart
@@ -8,17 +8,33 @@ class SecondScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-	return SafeArea(
-	  child: Scaffold(
-		body: Center(
-		  child: Column(
-			children: [
-			  Text(quoteText),
-			  Text(authorName),
-			],
-		  ),
-		),
-	  ),
-	);
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                quoteText,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              Text(
+                authorName,
+                style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Back to Home'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
